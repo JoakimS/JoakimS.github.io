a7f52739602f0d415ba877a632b4e70cd6650ff5
JoakimS <126427499+JoakimS@users.noreply.github.com>
2025-07-08 15:49:03 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -6,7 +6,7 @@ menuIcon.addEventListener('click', () => {
   navbar.classList.toggle('active');
 });
 
-const storyText = "Welcome! I am a Computer Engineer and Full Stack Developer, and my portfolio showcases a range of applications, from dynamic front-end designs to robust back-end systems.";
+const storyText = "Welcome! I am a Computer Engineer and Full Stack Developer. My portfolio showcases a range of applications—from dynamic front-end designs to robust back-end systems—built using technologies such as HTML, CSS, JavaScript, React, and Node.js. I have strong command of Data Structures and Algorithms (DSA), and experience with both relational (SQLite, MySQL) and non-relational (MongoDB) databases. I also work comfortably with C and C++, and use shell scripting for automation and system-level tasks.";
 const storyElement = document.getElementById("story-text");
 let index = 0;
 
